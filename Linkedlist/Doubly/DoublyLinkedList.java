99e25dadcf958a386f56ac1659576652b5ba64b2
Pramod Thorat <pramodthorat410@gmail.com>
2025-06-23 21:58:51 +0530

    Refactor deleteFront and deleteEnd methods for improved readability

--- Linkedlist/Doubly/DoublyLinkedList.java
+++ Linkedlist/Doubly/DoublyLinkedList.java
@@ -45,45 +45,45 @@ public class DoublyLinkedList {
 
     // Delete at the front
 
-    // public void deleteFront() {
-    //     if (head == null) {
-    //         System.out.println("List is Empty !!");
-    //         return;
-    //     }
-    //     if (head.next == null) {
-    //         head = null;
-    //         tail = null;
+    public void deleteFront() {
+        if (head == null) {
+            System.out.println("List is Empty !!");
+            return;
+        }
+        if (head.next == null) {
+            head = null;
+            tail = null;
 
-    //     } else {
-    //         head = head.next;
-    //         head.prev = null;
-    //     }
-    // }
+        } else {
+            head = head.next;
+            head.prev = null;
+        }
+    }
 
-    // public void deleteEnd() {
-    //     if (tail == null) {
-    //         System.out.println("List is Empty!!");
-    //     }
+    public void deleteEnd() {
+        if (tail == null) {
+            System.out.println("List is Empty!!");
+        }
 
-    //     if (tail.prev == null) {
-    //         head = tail = null;
-    //     } else {
-    //         tail = tail.prev;
-    //         tail.next = null;
-    //     }
-    // }
+        if (tail.prev == null) {
+            head = tail = null;
+        } else {
+            tail = tail.prev;
+            tail.next = null;
+        }
+    }
 
     
     // Display Backward 
 
-    // public void displayBackward() {
-    //     Node current = tail;
-    //     while (current != null) {
-    //         System.out.print(current.data + " ");
-    //         current = current.prev;
-    //     }
-    //     System.out.println();
-    // }
+    public void displayBackward() {
+        Node current = tail;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.prev;
+        }
+        System.out.println();
+    }
 
     // Display Forward
 
