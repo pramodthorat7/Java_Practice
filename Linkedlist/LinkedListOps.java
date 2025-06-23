0da390c0d3b5c80b98d7f547591cd79126652a76
Pramod Thorat <pramodthorat410@gmail.com>
2025-06-23 17:39:03 +0530

    Add menu options for linked list operations in Main class

--- Linkedlist/LinkedListOps.java
+++ Linkedlist/LinkedListOps.java
@@ -21,7 +21,8 @@ public class LinkedListOps {
     }
     
     // Insert data at the last.
-
+    // → Last Insert Operation is depend on the length of the linked list. So the complexity is O(n).
+   
     public void insertAtLast(int item) {
         Node newNode = new Node(item);
 
@@ -40,6 +41,8 @@ public class LinkedListOps {
     
 
     // Insert at Specific Position
+
+    // Operations : depend on the lenght of the list. So the complexity is O(n).
 
     public void insertAtPosition(int item, int position) {
         Node newNode = new Node(item);
@@ -70,15 +73,104 @@ public class LinkedListOps {
     }
     
 
+
+    // Print the Linked List Data
+    // Operations : O(n) - Linear Time Complexity due to depends on the length of the linked list.
+
+
     public void printData() {
         Node temp = head;
         if (temp == null) {
             System.out.print("Linked List is Empty! ");
         }
         while (temp != null) {
-            System.out.print( " ==> "+  temp.data);
+            System.out.print(" ==> " + temp.data);
             temp = temp.next;
         }
         System.out.println();
+    }
+    
+
+
+    // Delete From the begining 
+
+    // Operations : O(1) - Constant Time Complexity because we are just changing the head pointer.
+
+    public void deleteAtBegin() {
+        if (head == null) {
+            System.out.println("Linked List is Empty!");
+        }
+        head = head.next;
+    }
+
+
+    // Delete from the last
+    // Operations : O(n) - Linear Time Complexity because we are traversing the linked list to find the last node.
+
+
+    public void deleteAtLast() {
+        if (head == null) {
+            System.out.println("Linked List is null");
+        }
+
+        // for single element linked list
+        if (head.next == null) {
+            head = null;
+        }
+        // traverse and reach element
+        Node temp = head;
+        while (temp.next.next != null) {
+            temp = temp.next;
+        }
+        temp.next = null;
+    }
+
+    // Delete from specific Location
+    // Operations : O(n) - Linear Time Complexity because we are traversing the linked list to find the specific position.
+
+    public void deleteAtPosition(int position) {
+        if (head == null) {
+            System.out.println("List is empty !!");
+            return;
+        }
+
+        if (position == 0) {
+            head = head.next;
+            return;
+        }
+
+        Node temp = head;
+
+        for (int i = 0; i < position - 1 && temp != null; i++) {
+            temp = temp.next;
+            if (temp == null || temp.next == null) {
+                System.out.println("Position out of bounds.");
+                return;
+            }
+            temp.next = temp.next.next;
+        }
+    }
+
+
+    // Search Data in the Linked List
+    // Operations : O(n) - Linear Time Complexity because we are traversing the linked list to find the data.
+    // Best case: O(1) - If the data is found at the head.
+
+    
+    public boolean searchData(int data) {
+        
+        Node temp = head;
+        
+        while (temp != null) {
+            boolean result = false;
+            if (temp.data == data) {
+                // System.out.println("Data found in the Linked List");
+                return true;
+            }
+            temp = temp.next;
+
+        }
+        return false;
+
     }
 }
--- Linkedlist/Main.java
+++ Linkedlist/Main.java
@@ -1,21 +1,144 @@
 package Linkedlist;
 
-// import java.util.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        
 
+    public static void printMenu() {
+        System.out.println("***************MENU***************");
+        System.out.println("Enter 1 to INSERT ");
+        System.out.println("Enter 2 to DELETE ");
+        System.out.println("Enter 3 to SEARCH ");
+        System.out.println("Enter 4 to DISPLAY ");
+        System.out.println("Enter 5 to EXIT ");
+
+        System.out.println("Enter your choice: ");
+        System.out.println("**********************************");
+    }
+    
+    public static void printInsertMenu() {
+        System.out.println("Press 1 to insert at head");
+        System.out.println("Press 2 to insert at tail");
+        System.out.println("Press 3 to insert at a specific position");
+    }
+
+    public static void printDeleteMenu() {
+        System.out.println("Press 1 to delete at head");
+        System.out.println("Press 2 to delete at tail");
+        System.out.println("Press 3 to delete at a specific position");
+        
+    }
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println("**********Welcome to Linked List App**********");
         LinkedListOps linkedlist = new LinkedListOps();
 
-        linkedlist.insertAtBegin(10);
-        linkedlist.insertAtLast(20);
-        linkedlist.insertAtLast(30);
-        linkedlist.insertAtLast(40);
-        linkedlist.insertAtBegin(5);
-        linkedlist.insertAtPosition(4,0);
-        
-        linkedlist.printData();
-        
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            while (true) {
+                printMenu();
+                int choice = sc.nextInt();
+
+                switch (choice) {
+                    case 1:
+                        insertLinkedList(linkedlist);
+                        break;
+                
+                    case 2:
+                        deleteLinkedList(linkedlist);
+                        break;
+
+                    case 3:
+                        System.out.println("Enter the data your want to search: ");
+                        int data = sc.nextInt();
+
+                        boolean result = linkedlist.searchData(data);
+                        if (result) {
+                            System.out.println("Data found in the linked list.");
+                        } else {
+                            System.out.println("Data not found in the linked list.");
+                        }
+                        break;
+                    
+                    case 4:
+                        linkedlist.printData();
+                        break;
+                    
+                    case 5:
+                        System.out.println("Thank you for usign APP!!!");
+                        System.exit(0);       
+                        break;
+                    
+                    default:
+                        System.out.println("Invalid Choice...");
+
+                }
+            }
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    public static void insertLinkedList(LinkedListOps linkedlist) {
+        printInsertMenu();
+        Scanner sc = new Scanner(System.in);
+
+        int choice = sc.nextInt();
+        switch (choice) {
+            case 1:
+                System.out.println("Enter the data : ");
+                int data = sc.nextInt();
+                linkedlist.insertAtBegin(data);
+                break;
+
+            case 2:
+                System.out.println("Enter the data : ");
+                int data1 = sc.nextInt();
+                linkedlist.insertAtLast(data1);
+                break;
+
+            case 3:
+                System.out.println("Enter the data : ");
+                int data2 = sc.nextInt();
+                System.out.println("Enter the Position: ");
+                int pos = sc.nextInt();
+                linkedlist.insertAtPosition(data2, pos);
+
+            default:
+                System.out.println("Invalid Choice...");
+                break;
+        }
+    }
+    
+    private static void deleteLinkedList(LinkedListOps linkedList) {
+
+        printDeleteMenu();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter delete Choice ??");
+        int deleteChoice = scanner.nextInt();
+        if (deleteChoice == 1) {
+            linkedList.deleteAtBegin();
+            System.out.println("Deleted !!");
+        } 
+        else if (deleteChoice == 2) {
+            linkedList.deleteAtLast();
+            System.out.println("Deleted !!");
+        } 
+        else if (deleteChoice == 3) {
+            System.out.println("Enter the position :");
+            int position = scanner.nextInt();
+            linkedList.deleteAtPosition(position);
+            System.out.println("Deleted !!");
+        } 
+        else {
+            System.out.println("Invalid Delete Choice !");
+        }
+
     }
 }
