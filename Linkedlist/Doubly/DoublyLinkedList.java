c6d631338f74551f7ce8774f33c37250e0ef4041
Pramod Thorat <pramodthorat410@gmail.com>
2025-06-23 21:56:09 +0530

    Implement DoublyLinkedList and Node classes with methods to add elements at the front and end

---
+++ Linkedlist/Doubly/DoublyLinkedList.java
@@ -0,0 +1,100 @@
+package Linkedlist.Doubly;
+
+public class DoublyLinkedList {
+
+    private Node head;
+    private Node tail;
+
+    public DoublyLinkedList() {
+        head = null;
+        tail = null;
+
+    }
+
+    // Add the data to the front 
+
+    public void addFront(int data) {
+
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+    }
+
+    // Add the data to the Last
+
+    public void addEnd(int data) {
+
+        Node newNode = new Node(data);
+        if (tail == null) {
+            head = tail = newNode;
+        } else {
+            newNode.prev = tail;
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    // Add the data at Position
+
+    // Delete at the front
+
+    // public void deleteFront() {
+    //     if (head == null) {
+    //         System.out.println("List is Empty !!");
+    //         return;
+    //     }
+    //     if (head.next == null) {
+    //         head = null;
+    //         tail = null;
+
+    //     } else {
+    //         head = head.next;
+    //         head.prev = null;
+    //     }
+    // }
+
+    // public void deleteEnd() {
+    //     if (tail == null) {
+    //         System.out.println("List is Empty!!");
+    //     }
+
+    //     if (tail.prev == null) {
+    //         head = tail = null;
+    //     } else {
+    //         tail = tail.prev;
+    //         tail.next = null;
+    //     }
+    // }
+
+    
+    // Display Backword 
+
+    // public void displayBackward() {
+    //     Node current = tail;
+    //     while (current != null) {
+    //         System.out.print(current.data + " ");
+    //         current = current.prev;
+    //     }
+    //     System.out.println();
+    // }
+
+    // Display Forward
+
+    // public void displayForward() {
+    //     Node current = head;
+    //     while (current != null) {
+    //         System.out.print(current.data + " ");
+    //         current = current.next;
+    //     }
+    //     System.out.println();
+    // }
+
+}
+
---
+++ Linkedlist/Doubly/Node.java
@@ -0,0 +1,14 @@
+package Linkedlist.Doubly;
+
+public class Node {
+    
+    int data;
+    Node next;
+    Node prev;
+
+    public Node (int data){
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
+}
