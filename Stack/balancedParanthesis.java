ad522c6c86aa5e7a84e7cc4f0a4d1f14384a0b9d
Pramod Thorat <pramodthorat410@gmail.com>
2025-06-30 15:12:55 +0530

    Problem2

---
+++ Stack/balancedParanthesis.java
@@ -0,0 +1,37 @@
+package Stack;
+
+public class balancedParanthesis {
+    public static boolean isBalanced(String experession) {
+        
+        Stack stack = new Stack(experession.length());
+
+        for (int i = 0; i < experession.length(); i++) {
+            char ch = experession.charAt(i);
+
+            if (ch == '{' || ch == '[' || ch == '(') {
+                stack.push(ch);
+            } else if (ch == '}' || ch == ']' || ch == ')') {
+                
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = ((char) stack.pop());
+                
+                if ((top == '{' && ch == '}' || top == '[' && ch == ']' || top == '(' && ch == ')' )) {
+                    return true;
+                } else
+                    return false;
+                
+            }
+        }
+        return stack.isEmpty();
+    }
+
+
+
+    public static void main(String[] args) {
+        
+        System.out.println(isBalanced("((1{3[]47})9)"));
+    }
+}
